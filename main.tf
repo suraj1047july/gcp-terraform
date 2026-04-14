dd2ea395f3bb93b9e1a4b04efd3751e45a413c44
suraj1047july <122980832+suraj1047july@users.noreply.github.com>
2026-04-14 15:43:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,29 +14,29 @@ provider "google" {
 }
 
 # Create a Compute Instance
-resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-gcp-vm"
-  machine_type = "e2-micro" # Free-tier eligible
-  zone         = "asia-south2-a"
+#resource "google_compute_instance" "vm_instance" {
+# name         = "terraform-gcp-vm"
+ # machine_type = "e2-micro" # Free-tier eligible
+  #zone         = "asia-south2-a"
 
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
+  #boot_disk {
+    #initialize_params {
+     # image = "debian-cloud/debian-11"
+    #}
+  #}
 
-  network_interface {
+  #network_interface {
     # This attaches the VM to the "default" VPC network
-    network = "default"
+   # network = "default"
 
     # Access config block allows the VM to have a public IP
-    access_config {
-    }
-  }
+    #access_config {
+    #}
+  #}
 
-  metadata = {
-    foo = "bar"
-  }
+  # metadata = {
+    #foo = "bar"
+  #}
 
   tags = ["web-server", "terraform"]
 }
